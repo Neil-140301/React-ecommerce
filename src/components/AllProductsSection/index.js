888750711b7313d18153d3b34ce0d388e2d84986
Neil-140301 <alvares.1@iitj.ac.in>
2021-05-26 14:51:49 +0000

    react app

---
+++ src/components/AllProductsSection/index.js
@@ -0,0 +1,30 @@
+import {Component} from 'react'
+
+import ProductCard from '../ProductCard'
+import './index.css'
+
+class AllProductsSection extends Component {
+  state = {
+    productsList: [],
+  }
+
+  renderProductsList = () => {
+    const {productsList} = this.state
+    return (
+      <div>
+        <h1 className="products-list-heading">All Products</h1>
+        <ul className="products-list">
+          {productsList.map(product => (
+            <ProductCard productData={product} key={product.id} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  render() {
+    return <>{this.renderProductsList()}</>
+  }
+}
+
+export default AllProductsSection
